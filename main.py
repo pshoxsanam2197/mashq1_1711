bec525d53b7980022be44f4ef0429b6604ceaf0e
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2025-11-17 15:27:20 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+# 1-misol
+s = "abcdefghij"
+print(s)
+
+toza = s[2:5:1]
+print(toza)
+
+# 2-misol
+s = "HelloWorld"
+print(s)
+
+toza = s[0:5]
+print(toza)
+
+# 3-misol
+s = "PythonProgramming"
+print(s)
+
+toza = s[-7:]
+print(toza)
+
+# 4-misol
+s = "ABCDEFGHIJKLM"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+# 5-misol
+s = "0123456789"
+print(s)
+
+toza = s[::2]
+print(toza)
+
+# 6-misol
+s = "WelcomeToPython"
+print(s)
+
+toza = s[3:9]
+print(toza)
+
+# 7-misol
+s = "PythonLanguage"
+print(s)
+
+t = s[3:10]
+print(t)
+
+# 8-misol
+s = ("ABCDEFGHIJKLM")
+print(s)
+
+t = s[5:10]
+print(t)
+
+# 9-misol
+s = "superprogrammer"
+print(s)
+
+t = s[::-4]
+print(t)
+
+# 10-misol
+s = "0123456789"
+print(s)
+
+t = s[2:10:2]
+print(t)
